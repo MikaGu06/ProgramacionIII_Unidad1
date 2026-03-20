a66b4de2e64f8b90e9bd909b74244fe90278d9c7
diego_dell <diegoaprende06@gmail.com>
2026-03-19 23:29:32 -0400

    primerCOMMITdell
    xsd

--- ProgramacionIII_Unidad1/Inicio.xaml.cs
+++ ProgramacionIII_Unidad1/Inicio.xaml.cs
@@ -30,6 +30,13 @@ namespace ProgramacionIII_Unidad1
             AlgoritmosDeOrdenamiento paginaOrdenamiento = new AlgoritmosDeOrdenamiento();
 
             this.NavigationService.Navigate(paginaOrdenamiento);
+        }
+
+        private void BtnAbrirBusqueda_Click(object sender, RoutedEventArgs e)
+        {
+            ((MainWindow)Application.Current.MainWindow).MainFrame.Navigate(new AlgoritmosDeBusqueda());
+        }
+
         private void EstructurasLinealesBtn_Click(object sender, RoutedEventArgs e)
         {
             ((MainWindow)Application.Current.MainWindow).MainFrame.Navigate(new EstructurasLineales());
